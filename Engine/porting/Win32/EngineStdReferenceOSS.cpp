5717db33d6e8a18efd097e737c34801109a7a8da
Salaron <breykin.alexey@gmail.com>
2020-01-19 19:56:47 +0500

    Engine: register lua HASH lib

--- Engine/porting/Win32/EngineStdReferenceOSS.cpp
+++ Engine/porting/Win32/EngineStdReferenceOSS.cpp
@@ -130,6 +130,7 @@ bool EngineTaskReference()
 #include "CKLBLuaLibENG.h"
 #include "CKLBLuaLibFONT.h"
 #include "CKLBLuaLibGL.h"
+#include "CKLBLuaLibHASH.h"
 #include "CKLBLuaLibKEY.h"
 #include "CKLBLuaLibLANG.h"
 #include "CKLBLuaLibMatrix.h"
@@ -159,6 +160,7 @@ bool EngineLuaFuncReference()
 	bResult = bResult && getFuncRegister<CKLBLuaLibENG>();
 	bResult = bResult && getFuncRegister<CKLBLuaLibFONT>();
 	bResult = bResult && getFuncRegister<CKLBLuaLibGL>();
+	bResult = bResult && getFuncRegister<CKLBLuaLibHASH>();
 	bResult = bResult && getFuncRegister<CKLBLuaLibKEY>();
 	bResult = bResult && getFuncRegister<CKLBLuaLibLANG>();
 	bResult = bResult && getFuncRegister<CKLBLuaLibMatrix>();
